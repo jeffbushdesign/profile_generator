6f2fb7b8ff7e30723d2e4ca8c979c0e3887df58b
jeffbushdesign <jeffbushdesign@gmail.com>
2022-04-19 22:09:06 -0400

    Complete survey using command line

--- survey.js
+++ survey.js
@@ -5,10 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+  rl.question('What\s an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (music) => {
+      rl.question('Which meal is your favourite? (eg: dinner, brunch, etc.) ', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              console.log(`My name is ${name.trim()}, and I love ${activity.trim()} (while listening to ${music.trim()}). My favourite food is ${food.trim()} for ${meal.trim()}, my favourite sport is ${sport.trim()}, and my (not-so-secret) power is ${superpower.trim()}.`);
+              rl.close();
+            })
+          })
+        })
+      })
+    })
+  })
 });
 
 /*
